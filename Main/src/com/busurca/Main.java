3396977e7d06d87b9896a474dbf5d458e7870ca1
AlexBus <alexandru.busurca@gmail.com>
2020-03-12 09:32:20 +0200

    did lesson on boxin(auto and un)

--- Main/src/com/busurca/Main.java
+++ Main/src/com/busurca/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
-    private static GroceryList gl = new GroceryList();
+    //private static GroceryList gl = new GroceryList();
 
     public static void main(String[] args) {
         boolean quit = false;
